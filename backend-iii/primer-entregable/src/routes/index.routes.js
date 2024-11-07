2455741407b050117e8248c78db0680c67ea1e0a
brando1607 <109045330+brando1607@users.noreply.github.com>
2024-11-07 11:53:56 -0300

    index router imports fixed

--- backend-iii/primer-entregable/src/routes/index.routes.js
+++ backend-iii/primer-entregable/src/routes/index.routes.js
@@ -1,8 +1,8 @@
-import { authRouter } from "./routes/auth.routes.js";
-import { userRouter } from "./routes/user.routes.js";
-import { cartRouter } from "./routes/cart.routes.js";
-import { mocksRouter } from "./routes/mocks.routes.js";
-import { productsRouter } from "./routes/products.routes.js";
+import { authRouter } from "./auth.routes.js";
+import { userRouter } from "./user.routes.js";
+import { cartRouter } from "./cart.routes.js";
+import { mocksRouter } from "./mocks.routes.js";
+import { productsRouter } from "./products.routes.js";
 
 export class RouterIndex {
   static authRouter = authRouter;
